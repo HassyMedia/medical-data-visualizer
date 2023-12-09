3f1799d5213b6532c142180ca4968c2fb23ef40a
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-09 14:10:01 -0500

    Implemented calculation of BMI and added an 'overweight' column to the dataframe.

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -5,3 +5,6 @@ import seaborn as sns
 # Load the data
 df = pd.read_csv('medical_examination.csv')
 
+# Add 'overweight' column
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype(int)
+
