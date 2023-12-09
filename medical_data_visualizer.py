6c625e0afecb400f184a21dda7f0dd32f2544701
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-09 14:17:39 -0500

    Implemented data cleaning by filtering out incorrect data based on specified conditions.

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -22,3 +22,13 @@ def draw_cat_plot():
 
 draw_cat_plot()
 
+# Clean the data
+df_clean = df[
+    (df['ap_lo'] <= df['ap_hi']) &
+    (df['height'] >= df['height'].quantile(0.025)) &
+    (df['height'] <= df['height'].quantile(0.975)) &
+    (df['weight'] >= df['weight'].quantile(0.025)) &
+    (df['weight'] <= df['weight'].quantile(0.975))
+]
+
+
